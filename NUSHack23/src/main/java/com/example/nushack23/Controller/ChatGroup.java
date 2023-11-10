a82603531f066a1e66762ccf132e54a1a672f0b1
elena-ignaev <ngkhanhvy11@gmail.com>
2023-11-10 20:25:53 +0800

    more chatgroup UI

--- NUSHack23/src/main/java/com/example/nushack23/Controller/ChatGroup.java
+++ NUSHack23/src/main/java/com/example/nushack23/Controller/ChatGroup.java
@@ -1,4 +1,14 @@
 package com.example.nushack23.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ChatGroup {
+    @FXML
+    private Button joinGroup;
+    @FXML
+    private Button send;
+    @FXML
+    private Button addFunctions;
+
 }
