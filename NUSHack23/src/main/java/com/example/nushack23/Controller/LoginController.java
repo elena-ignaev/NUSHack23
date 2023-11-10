1926b47ea7555470a7fb01d05f8aa57d6fffe9ca
livvyaaa <violetyy.accounts@skiff.com>
2023-11-10 22:48:32 +0800

    login controller update

--- NUSHack23/src/main/java/com/example/nushack23/Controller/LoginController.java
+++ NUSHack23/src/main/java/com/example/nushack23/Controller/LoginController.java
@@ -1,4 +1,36 @@
 package com.example.nushack23.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class LoginController {
+
+    @FXML
+    private Button createNewAccButton;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private ImageView loginPageImageView;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    void createNewAcc(ActionEvent event) {
+
+    }
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
 }
