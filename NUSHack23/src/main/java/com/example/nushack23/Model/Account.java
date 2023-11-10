e0e05a061d55a09f6ed8c257c0e36c88b1b59e59
Simul-Eqn <89591030+Simul-Eqn@users.noreply.github.com>
2023-11-10 17:53:43 +0800

    start

--- NUSHack23/src/main/java/com/example/nushack23/Model/Account.java
+++ NUSHack23/src/main/java/com/example/nushack23/Model/Account.java
@@ -1,4 +1,38 @@
 package com.example.nushack23.Model;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Account {
+    private static ArrayList<Account> accounts = new ArrayList<>();
+    private static ArrayList<String> usernames = new ArrayList<>();
+    private static ArrayList<String> passwordHashes = new ArrayList<>();
+    private String username, pwdHash;
+
+    public String getUsername() {
+        return username;
+    }
+    public String getPwdHash() { return pwdHash; }
+    public static ArrayList<Account> getAccounts() {
+        return accounts;
+    }
+
+    public static Account login(String username, String password) {
+        for (int i=0; i<usernames.size(); i++) {
+            if (Objects.equals(usernames.get(i), username)) {
+                if (Objects.equals(passwordHashes.get(i), pwdHashFunc(password))) {
+                    return Account.accounts.get(i);
+                }
+            }
+        }
+        return null;
+    }
+
+
+    public static String pwdHashFunc(String pwd) {
+        String pwdHash = "";
+        pwdHash = pwd;
+        return pwdHash;
+    }
+
 }
---
+++ NUSHack23/src/main/java/com/example/nushack23/Model/Database.java
@@ -0,0 +1,11 @@
+package com.example.nushack23.Model;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Database {
+    private static ArrayList<Chat> all_chats = new ArrayList<>();
+
+
+
+}
